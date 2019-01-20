4481065cdeb332f8de3970d3cfed73e4f497fe1d
Richard Lee <rich.leeroy@gmail.com>
2019-01-19 19:08:06 -0500

    clean up

--- detection.js
+++ detection.js
@@ -1,26 +1,42 @@
-var UA = window.navigator.userAgent;
-var msie = UA.indexOf('MSIE ');
-var trident = UA.indexOf('Trident/');
-var edge = UA.indexOf('Edge/');
-var ie11 = UA.indexOf("Trident/7.0");
-var iPad = /iPad/i.test(UA);
-var iPhone = /iPhone/i.test(UA);
-var iOS = /iPhone/i.test(UA) || /iPod/i.test(UA) || /iPad/i.test(UA);
-var android = /(android)/i.test(navigator.userAgent);
+// Browser addDev - Variables
+var UA = window.navigator.userAgent,
+chrome = navigator.userAgent.search("Chrome"),
+firefox = navigator.userAgent.search("Firefox"),
+msie = UA.indexOf('MSIE '),
+trident = UA.indexOf('Trident/'),
+edge = UA.indexOf('Edge/'),
+ie11 = UA.indexOf("Trident/7.0"),
+iPad = /iPad/i.test(UA),
+iPhone = /iPhone/i.test(UA),
+iOS = /iPhone/i.test(UA) || /iPod/i.test(UA) || /iPad/i.test(UA),
+android = /(android)/i.test(navigator.userAgent);
 
-var doc = document.getElementsByTagName('html');
+var element, arr;
 
+element = document.getElementsByTagName('html')[0];
+arr = element.className.split(' ');
 
-if (msie > 0 || trident > 0 || edge > 0) {
-    $('html').addClass('isIE');
-    if (ie11 > 0) {
-        $('html').addClass('isIE11');
+var addDevClass = function (className) {
+    if (arr.indexOf(className) == -1) {
+      element.className += ' ' + className;
     }
+}
+
+// Browser addDev - Attach browser specific class to HMTL tag
+if (msie > 0 || trident > 0 || edge > 0) {
+    addDevClass('isIE');
+    if (ie11 > 0) {
+        addDevClass('isIE11');
+    }
+} else if (firefox > 0) {
+    addDevClass('isFF');
+} else if (chrome > 0) {
+    addDevClass('isChrome');
 } else if (iOS > 0) {
-    $('html').addClass('isIOS');
-    if (iOS == /iPad/i.test(UA)) {
-        $('html').addClass('isIpad');
+    addDevClass('isIOS');
+    if (iPad > 0) {
+        addDevClass('isIpad');
     }
 } else if (android > 0) {
-    $('html').addClass('isAndroid');
+    addDevClass('isAndroid');
 }
